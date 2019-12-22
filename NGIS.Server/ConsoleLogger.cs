4a22c97378e024acaaf2803950d81e32e92ec14c
Danil Gulin <dngulin@yandex.ru>
2019-12-23 02:51:18 +0300

    Add date & time to logger

--- NGIS.Server/ConsoleLogger.cs
+++ NGIS.Server/ConsoleLogger.cs
@@ -3,12 +3,12 @@ using NGIS.Logging;
 
 namespace NGIS.Server {
   public class ConsoleLogger : ILogger {
-    public void Info(string msg) => Console.WriteLine($"[INFO] {msg}");
-    public void Warning(string msg) => Console.WriteLine($"[WARNING] {msg}");
-    public void Error(string msg) => Console.WriteLine($"[ERROR] {msg}");
+    public void Info(string msg) => Console.WriteLine($"{DateTime.Now} [INFO] {msg}");
+    public void Warning(string msg) => Console.WriteLine($"{DateTime.Now} [WARNING] {msg}");
+    public void Error(string msg) => Console.WriteLine($"{DateTime.Now} [ERROR] {msg}");
 
     public void Exception(Exception e) {
-      Console.WriteLine("[EXCEPTION]");
+      Console.WriteLine($"{DateTime.Now} [EXCEPTION]");
       Console.WriteLine(e);
     }
   }
